fcf3eba540b211795fa9acecd8f6e9b985188dd1
vivek-anand-singh <17vivekanandsingh@gmail.com>
2024-07-08 11:17:28 +0530

    First Commit

--- index.js
+++ index.js
@@ -23,54 +23,6 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });   
 
-// Create
-
-app.post("/api/products", async (req, res) => {
-   await ProductModel.create({
-    product_name: req.body.product_name,
-    product_price: req.body.product_price,
-    isInStock: req.body.isInStock,
-    category: req.body.category,
-  });
-
-  return res.status(201).json({ message: "Product Created" });
-});
-
-
-// get route
-
-app.get('/api/products' , async(req , res)=>{
-   const allProucts = await ProductModel.find({isInStock:true})
-
-   return res.json(allProucts)
-})
-
-// Get product by id
-
-app.get('/api/products/:id' , async(req , res)=>{
- const product = await ProductModel.findById(req.params.id)
-
- return res.json(product)
-})
-
-// Update product
-
-app.put('/api/products/:id' , async(req , res)=>{
-  const updatedProduct = await ProductModel.findByIdAndUpdate(req.params.id , req.body)
-  return res.json(updatedProduct)
-})
-
-
-/// Delete a Resource
-
-app.delete('/api/products/:id' , async(req , res)=>{
-  const deletedProduct = await ProductModel.findByIdAndDelete(req.params.id)
-
-  res.json(deletedProduct)
-})
-
-
-
 app.listen(8086, () => {
   console.log("Server is live at http://localhost:8086")
 });
